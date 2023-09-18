36b80f3987fd2e44ee528487fcb79b1097654183
Matheus Bergamo <matheusbergamo@hotmail.com>
2023-09-18 14:38:05 -0300

    deploy

--- blogpessoal/src/main/java/com/generation/blogpessoal/Repository/TemaRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/Repository/TemaRepository.java
@@ -3,9 +3,11 @@ package com.generation.blogpessoal.Repository;
 import com.generation.blogpessoal.Models.Tema;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TemaRepository extends JpaRepository<Tema, Long> {
     public List<Tema> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
 }
